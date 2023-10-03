e4d6104ffbbe66c5b3854c48b1099665c52c3fc8
LaylaR <layla.richard.2111@gmail.com>
2023-10-03 13:52:14 +0200

    compute and display remaining days used at creation used as refresh

---
+++ scripts/remainingDays.js
@@ -0,0 +1,38 @@
+﻿import {tasks} from "./task.js";
+
+refreshRemainingDays();
+function refreshRemainingDays(){
+    let tasksElements = document.querySelectorAll(".task");
+
+    for(let element of tasksElements){
+
+        //Find currentCard in js collection based on id     
+        let task = GetCard(Number(element.id));
+        
+        //Display remaining Time in html
+        DisplayRemainingDays(task.id,ComputeRemainingDays(task.dueDate));
+    }
+}
+
+function GetCard(id){
+    return tasks.find(t => t.id === id);
+}
+
+function ComputeRemainingDays(dueDate){
+
+    return Math.floor(Math.abs(dueDate-Date.now()) / (1000 * 60 * 60 * 24));
+  
+}
+
+function DisplayRemainingDays(taskId,time){
+    
+    let currentElement = document.getElementById(taskId);
+    if(currentElement !== null && currentElement !== undefined){
+        
+        let remainingDaysElement = currentElement.querySelector(".dueDate__Text");
+        remainingDaysElement.innerText = `due in ${time} days`;        
+    }
+    
+}
+
+export {ComputeRemainingDays};
--- scripts/task.js
+++ scripts/task.js
@@ -1,13 +1,15 @@
-
+import {ComputeRemainingDays} from "./remainingDays.js";
 
 class task {
-    constructor(title, status, dueDate, description,id ) {
-    this.title = title;
-    this.status = status;
-    this.dueDate = dueDate;
-    this.description = description;
-    this.id = id;
+    constructor(title, status, dueDate, description, id) {
+        this.title = title;
+        this.status = status;
+        this.dueDate = dueDate;
+        this.description = description;
+        this.id = id;
+        this.remainingDays = ComputeRemainingDays(dueDate);
     }
-    }
+}
+let tasks = [];
 
-    let tasks = [];
+export {tasks};
