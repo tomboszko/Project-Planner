a6a7da360a22c112635f51195395ef14be729607
LaylaR <layla.richard.2111@gmail.com>
2023-10-03 15:17:18 +0200

    remaining days -  check for null or empty

--- scripts/remainingDays.js
+++ scripts/remainingDays.js
@@ -1,43 +1,49 @@
 ﻿import {tasks} from "./task.js";
 
-function RefreshRemainingDays(){
+function RefreshRemainingDays() {
     let tasksElements = document.querySelectorAll(".task");
 
-    for(let element of tasksElements){
+    if (tasksElements !== null && tasksElements !== undefined && tasksElements.length > 0) {
+        for (let element of tasksElements) {
 
-        //Find currentCard in js collection based on id     
-        let task = GetCard(Number(element.id));
-        
-        //Display remaining Time in html
-        DisplayRemainingDays(task.id,ComputeRemainingDays(task.dueDate));
+            //Find currentCard in js collection based on id     
+            let task = GetCard(Number(element.id));
+
+            if (task !== null && task !== undefined) {
+                
+                //Compute and Display remaining Time in html
+
+                DisplayRemainingDays(task.id, ComputeRemainingDays(task.dueDate));
+            }
+
+        }
     }
+
 }
 
-function GetCard(id){
+function GetCard(id) {
     return tasks.find(t => t.id === id);
 }
 
-function ComputeRemainingDays(dueDate){
+function ComputeRemainingDays(dueDate) {
 
-    if(dueDate.getTime()>Date.now()){
-        return Math.ceil(Math.abs(dueDate-Date.now()) / (1000 * 60 * 60 * 24));
-    }
-    
-    else{
+    if (dueDate.getTime() > Date.now()) {
+        return Math.ceil(Math.abs(dueDate - Date.now()) / (1000 * 60 * 60 * 24));
+    } else {
         return 0;
-    }    
-  
+    }
+
 }
 
-function DisplayRemainingDays(taskId,time){
-    
+function DisplayRemainingDays(taskId, time) {
+
     let currentElement = document.getElementById(taskId);
-    if(currentElement !== null && currentElement !== undefined){
-        
+    if (currentElement !== null && currentElement !== undefined) {
+
         let remainingDaysElement = currentElement.querySelector(".remainingTime___Text");
-        remainingDaysElement.innerText = `${time} remaining days`;        
+        remainingDaysElement.innerText = `${time} remaining days`;
     }
-    
+
 }
 
 export {ComputeRemainingDays, RefreshRemainingDays};
--- scripts/task.js
+++ scripts/task.js
@@ -12,4 +12,8 @@ class task {
 }
 let tasks = [];
 
+tasks.push(
+    new task("title1","todo",new Date(2023,10,3),"blablabla",1)
+)
+
 export {tasks};
