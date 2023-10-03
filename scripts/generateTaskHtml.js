4089c4a6a662174f7b84155d8326fc09517b0406
LaylaR <layla.richard.2111@gmail.com>
2023-10-03 15:36:12 +0200

    safer generate html code

--- scripts/generateTaskHtml.js
+++ scripts/generateTaskHtml.js
@@ -3,23 +3,30 @@
     taskCard.classList.add("card","task");
     taskCard.setAttribute("id",task.id);
     taskCard.setAttribute("style","width: 18rem;");
-    taskCard.innerHTML = FulFillTaskHtml(task);
+    taskCard.innerHTML = template;
+    FulFillTaskHtml(taskCard,task);
     return taskCard;
 }
-function FulFillTaskHtml(task){
-    
-    return `<div class="card-header">
-                        <h5 id="titleCard1" class="card-title">${task.title}</h5>
-                        <span id="status__Card1" class="badge text-bg-primary">${task.status}</span>
-                        <p id="remainingTime__Card1" class="remainingTime___Text">${task.remainingDays} remaining days</p>
+
+let template = `<div class="card-header">
+                        <h5 id="titleCard1" class="card-title taskTitle"></h5>
+                        <span id="status__Card1" class="badge text-bg-primary taskStatus"></span>
+                        <p id="remainingTime__Card1" class="remainingTime___Text"></p>
                     </div>
                     <div id="bodyCard1" class="card-body">
-                        <p id="descriptionCard1" class="card-text">${task.description}</p>
+                        <p id="descriptionCard1" class="card-text cardDescription"></p>
                     </div>
                     <div class="card-footer">
-                        <p id="dueDate__Card1" class="dueDate__Text">${task.dueDate.toDateString()}</p>
+                        <p id="dueDate__Card1" class="dueDate__Text"></p>
                         <button id="editBtn-Card1" type="button" class="btn editButton">Edit</button>
                     </div>`
+function FulFillTaskHtml(taskCard,task){
+    
+   taskCard.querySelector(".taskTitle").innerText = task.title;
+   taskCard.querySelector(".taskStatus").innerText = task.status;
+   taskCard.querySelector(".remainingTime___Text").innerText = task.remainingDays + "remaining days";
+   taskCard.querySelector(".cardDescription").innerText = task.description;
+   taskCard.querySelector(".dueDate__Text").innerText = task.dueDate.toDateString();
 }
 
 function AddTaskToColumn(task){
