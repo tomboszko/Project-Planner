0f1e5023e3f411a151a757dffb6f97ee38b9ad28
tomboszko <gomreethomas@gmail.com>
2023-10-03 12:00:57 +0200

    add directory scripts

---
+++ scripts/task.js
@@ -0,0 +1,13 @@
+
+
+class task {
+    constructor(title, status, dueDate, description,id ) {
+    this.title = title;
+    this.status = status;
+    this.dueDate = dueDate;
+    this.description = description;
+    this.id = id;
+    }
+    }
+
+    let tasks = [];
