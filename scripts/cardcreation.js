b5a9d118978719a0e92537f5c2bb55b38e2b54ad
tomboszko <gomreethomas@gmail.com>
2023-10-03 15:07:52 +0200

    maj script

--- scripts/cardcreation.js
+++ scripts/cardcreation.js
@@ -1,6 +1,6 @@
 
-
 function createTask(cardElement) {
+
     let title = cardElement.querySelector('.card-title').textContent;
     let status = cardElement.querySelector('.badge').textContent;
     let dueDate = cardElement.querySelector('.dueDate__Text').textContent;
@@ -10,18 +10,31 @@ function createTask(cardElement) {
     return new task(title, status, dueDate, description, id);
 }
 
-function addItemToDo() {
- 
-    document.getElementById('addToDo').addEventListener('click', function () {
 
-        document
-        
+function addTaskForm() {
 
-
-    
- 
-});
+    let titleInput = document.getElementById('card-titleInput');
+    let dueDateInput = document.getElementById('dueDate__Card1Input');
+    let descriptionInput = document.getElementById('descriptionCard1Input');
+    let statusInput =document.getElementById('status__Card1Input');
 
 
+    let title = titleInput.value;
+    let dueDate = dueDateInput.value;
+    let description = descriptionInput.value;
+    let status = statusInput.value;
+
+    let newTask = new task(title, status, dueDate, description, 'id');
+    tasks.push(newTask);
+
+    titleInput.value = '';
+    dueDateInput.value = '';
+    descriptionInput.value = '';
 }
+
+let addButton = document.querySelector('.addTask');
+addButton.addEventListener('click', addTaskForm());
+
+
+
 
