462e7e4173afad3748682f746ef6e4f2a922d57f
tomboszko <gomreethomas@gmail.com>
2023-10-05 16:06:51 +0200

    maj style cards

--- scripts/generateTaskHtml.js
+++ scripts/generateTaskHtml.js
@@ -11,22 +11,23 @@ function GenerateTaskHtml(task){
     return taskCard;
 }
 
-let template = `<div class="row g-0 mt-0">
-                        <div class="col-7">
+let template = `<div class="row g-0">
+                        <div class="col-10">
                             <div id="bodyCard1" class="card-body">
+
                                 <h5 id="titleCard1" class="card-title taskTitle"></h5>
-                                
-                                <p id="remainingTime__Card1" class="remainingTime___Text taskText"></p>
+
+                                <p id="remainingTime__Card1" class="remainingTime___Text taskText mb-0 text-left"></p>
+
                                 <p id="descriptionCard1" class="card-text cardDescription taskText"></p>
+
                             </div>
                         </div>
-                        <div class="col-3">
-                        <div class="wrapper dueTime d-flex flex-column">
+
                         
-                        
-                    </div></div>
+                    
+
                         <div class="col-2 text-end pe-0 d-flex flex-column justify-content-around align-items-end">
-                            
                             <span id="status__Card1" class="badge taskText taskStatus "></span>
                             <p id="dueDate__Card1" class="dueDate__Text taskText p-0 m-0"></p>
                             <button id="editBtn-Card1" type="button" class="btn editButton taskText align-self-end">Edit</button>
